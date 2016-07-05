06a9a9a449247191e7261bb427d6913f8022bb6b
Anuraag <anuraag@magnifi.fm>
2016-07-05 00:53:09 -0700

    Settings form saves data to local-storage
    Some code comments
    Basic error handling in form for required fields and min-value

--- app/scripts/controllers/settings.js
+++ app/scripts/controllers/settings.js
@@ -8,11 +8,21 @@
  * Controller of the coloredApp
  */
 angular.module('coloredApp')
-  .controller('SettingsCtrl', function ($scope, gameSettings) {
-    debugger;
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('SettingsCtrl', function($scope, gameSettingService, gameSettings) {
+    $scope.rows = gameSettings.rows;
+    $scope.cols = gameSettings.cols;
+    $scope.count = gameSettings.count;
+    $scope.attempts = gameSettings.attempts;
+    $scope.time = gameSettings.time;
+
+    $scope.saveSettings = function() {
+      gameSettingService.saveManySettings({
+        rows: $scope.rows,
+        cols: $scope.cols,
+        count: $scope.count,
+        attempts: $scope.attempts,
+        time: $scope.time
+      });
+    };
+
   });
--- app/scripts/services/gamesettingservice.js
+++ app/scripts/services/gamesettingservice.js
@@ -11,6 +11,7 @@ angular.module('coloredApp')
   .service('gameSettingService', function($q, localStorageService) {
     var settingKeyName = 'game_settings',
       currentSettings = {},
+      //Default values for a new user
       defaultSettings = {
         'rows': 4,
         'cols': 4,
@@ -22,6 +23,7 @@ angular.module('coloredApp')
     function _getAllSettings() {
       return $q(function(resolve, reject) {
         currentSettings = localStorageService.get(settingKeyName);
+        // if the value is not found - resort to setting a defaultSettings object
         if (!currentSettings) {
           localStorageService.set(settingKeyName, defaultSettings);
           // refetch from storage
@@ -31,9 +33,22 @@ angular.module('coloredApp')
       });
     }
 
+    //Save a setting key value pair to game settings
     function _saveSetting(key, value) {
       currentSettings[key] = value;
       localStorageService.set(settingKeyName, currentSettings);
+    }
+
+    /**
+     * Can save multiple settings together
+     * Over here we are iterating and calling localStorage update multiple times
+     * In ideal world,
+     * we will update to api and the api should update only the values it knows and recieves
+     */
+    function _saveManySettings(map) {
+      for (var key in map) {
+        _saveSetting(key, map[key]);
+      }
     }
 
     function _reset() {
@@ -41,9 +56,12 @@ angular.module('coloredApp')
       // refetch from storage
       currentSettings = localStorageService.get(settingKeyName);
     }
+
+    // This will expose what is needed for the service interface in the  app
     return {
       getAllSettings: _getAllSettings,
       reset: _reset,
-      saveSetting: _saveSetting
+      saveSetting: _saveSetting,
+      saveManySettings: _saveManySettings
     }
   });
